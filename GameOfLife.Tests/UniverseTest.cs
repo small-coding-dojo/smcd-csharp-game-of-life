e7a913fbd282bc7dc24d7fb6eccc115f6a49cdf6
Atcheri Atch <atcheri@gmail.com>
2023-02-02 18:34:52 +0100

    feat: we can create a universe with exactly one dead cell
    Co-authored-by: Holger Janssen-Kroll <holger.janssen-kroll@aleri.de>
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>
    Co-authored-by: Atsuhiro Endo <atcheri@gmail.com>

--- GameOfLife.Tests/UniverseTest.cs
+++ GameOfLife.Tests/UniverseTest.cs
@@ -5,11 +5,12 @@ namespace GameOfLife.Tests;
 
 public class UniverseTest
 {
-    private class UniverseAdapter: Universe {
-        
-        public UniverseAdapter(int rows, int cols): base(rows, cols) {}
+    private class UniverseAdapter : Universe
+    {
 
-        public int Cells  => _cells.Length;
+        public UniverseAdapter(int rows, int cols, bool initialCellState) : base(rows, cols, initialCellState) { }
+
+        public int Cells => _cells.Length;
 
         public Cell cellAt00 => _cells[0, 0];
     }
@@ -17,12 +18,17 @@ public class UniverseTest
     [Fact]
     public void CreatesUniverseWithOneAliveCell()
     {
-        var universe = new UniverseAdapter(1,1);
-        
+        var universe = new UniverseAdapter(1, 1, true);
+
         Assert.Equal(1, universe.Cells);
-        Assert.Equal(true,universe.cellAt00 is Cell);
-        Assert.Equal(true,universe.cellAt00.IsAlive());
+        Assert.Equal(true, universe.cellAt00 is Cell);
+        Assert.Equal(true, universe.cellAt00.IsAlive());
     }
 
-    
+    [Fact]
+    public void CreatesUniverseWithOneDeadCell()
+    {
+        var universe = new UniverseAdapter(1, 1, false);
+        Assert.Equal(false, universe.cellAt00.IsAlive());
+    }
 }
--- GameOfLife/Universe.cs
+++ GameOfLife/Universe.cs
@@ -2,9 +2,9 @@ namespace GameOfLife.Tests;
 
 public class Universe
 {
-    protected Cell[,] _cells = new Cell[1,1];
-    public Universe(int rows, int cols)
+    protected Cell[,] _cells = new Cell[1, 1];
+    public Universe(int rows, int cols, bool initialCellState)
     {
-        _cells[0,0] = new Cell(true);
+        _cells[0, 0] = new Cell(initialCellState);
     }
 }
