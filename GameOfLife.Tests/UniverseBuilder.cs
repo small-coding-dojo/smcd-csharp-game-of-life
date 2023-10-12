782977bae5ce8eee83b10c068286be1b49a4c266
Niklas Rueber <nrueber@igus.net>
2023-10-12 18:02:58 +0200

    docs: add documentation for what is an "alive" cell and what "dead"
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Holger Janssen-Kroll <hjanssenkroll@igus.net>
    Co-authored-by: Tugbay Atilla <tugbayatilla@gmail.com>

--- GameOfLife.Tests/UniverseBuilder.cs
+++ GameOfLife.Tests/UniverseBuilder.cs
@@ -2,11 +2,16 @@ namespace GameOfLife.Tests;
 
 public static class UniverseBuilder
 {
-    public static Universe Build(string s)
+    /// <summary>
+    /// Build a universe from a string representation.
+    /// </summary>
+    /// <param name="stringRepresentation">use . for dead and 0 for alive cells, e.g. "...O." means dead dead dead alive dead</param>
+    /// <returns>A universe as described by the parameter stringRepresentation</returns>
+    public static Universe Build(string stringRepresentation)
     {
         // rule disabled to prevent ternary operator (?:)
         // ReSharper disable once ConvertIfStatementToReturnStatement
-        if (s == "O")
+        if (stringRepresentation == "O")
         {
             return new Universe(1, 1, true);
         }
