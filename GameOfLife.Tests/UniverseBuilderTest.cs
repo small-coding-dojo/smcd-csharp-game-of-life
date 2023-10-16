f11d96db370cf8dac669daf983743f559b776d7b
Niklas Rueber <nrueber@igus.net>
2023-10-16 17:32:04 +0200

    test: Check universes for dimensions
    Co-authored-by: Stefan Boos <kontakt@boos.systems> Co-authored-by: Holger Janssen-Kroll <hjanssenkroll@igus.net> Co-authored-by: Niklas Rueber <nrueber@igus.net> Co-authored-by: Tugbay Atilla <tugbayatilla@gmail.com>  Co-authored-by: Michael Mahlberg <mm@consulting-guild.de> Co-authored-by: Atsuhiro Endo <atcheri@gmail.com>

--- GameOfLife.Tests/UniverseBuilderTest.cs
+++ GameOfLife.Tests/UniverseBuilderTest.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using System;
 
 namespace GameOfLife.Tests;
 
@@ -9,6 +10,7 @@ public class UniverseBuilderTest
     {
         var universe = UniverseBuilder.Build("");
         Assert.Equal(0, universe.CellsLength);
+        Assert.Throws<IndexOutOfRangeException>(() => (universe.CellAt(0, 0)));
     }
 
     [Fact]
@@ -17,5 +19,9 @@ public class UniverseBuilderTest
         var universe = UniverseBuilder.Build("O");
         Assert.Equal(1, universe.CellsLength);
         Assert.True(universe.CellAt(0, 0).IsAlive());
+        Assert.Throws<IndexOutOfRangeException>(() => (universe.CellAt(-1, 0)));
+        Assert.Throws<IndexOutOfRangeException>(() => (universe.CellAt(1, 0)));
+        Assert.Throws<IndexOutOfRangeException>(() => (universe.CellAt(0, -1)));
+        Assert.Throws<IndexOutOfRangeException>(() => (universe.CellAt(0, 1)));
     }
 }
