77bc4a2dfd6843270afc80b52b0bfcee903b8a93
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-11-02 18:05:16 +0100

    doc: show how the Builder shall be used
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>
    Co-authored-by: Atsuhiro Endo <atcheri@gmail.com>
    Co-authored-by: Stefan Boos <kontakt@boos.systems>

--- GameOfLife.Tests/UniverseBuilder.cs
+++ GameOfLife.Tests/UniverseBuilder.cs
@@ -5,7 +5,28 @@ public static class UniverseBuilder
     /// <summary>
     /// Build a universe from a string representation.
     /// </summary>
-    /// <param name="stringRepresentation">use . for dead and 0 for alive cells, e.g. "...O." means dead dead dead alive dead</param>
+    /// <remarks>
+    /// <para>
+    /// Purpose:
+    /// <list type="">
+    ///   <item>- The UniverseBuilder creates a new Universe every time the Build method is called.</item>
+    ///   <item>- This is intended to be used as an initial state for test cases.</item>
+    ///   <item>- The <c>Build</c> method should be called to setup the initial state for every test case.</item>
+    /// </list>
+    /// </para>
+    /// <para>
+    /// Info: <br/>
+    /// - Don't have any validity checks
+    /// </para>
+    /// <para>
+    ///     Example: <br/>
+    ///         ..O. <br/>
+    ///         O..O <br/>
+    /// -> means 2 by 4 universe with living cell on the 3rd position in the 1st row and living cells in 1st and 4th position in the 2nd row
+    /// </para>
+    /// </remarks>
+    ///
+    /// <param name="stringRepresentation">Use "." for dead and "O" for alive cells, e.g. "...O." means "dead dead dead alive dead"</param>
     /// <returns>A universe as described by the parameter stringRepresentation</returns>
     public static Universe Build(string stringRepresentation)
     {
@@ -26,7 +47,7 @@ public static class UniverseBuilder
         return new Universe(0, 0, false);
     }
 
-    public static bool desiredCellState (char input)
+    public static bool desiredCellState(char input)
     {
         return input == 'O' ? true : false;
     }
